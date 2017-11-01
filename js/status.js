b01cd47139c46624d9818654f0ebf6eda5983496
Sheepotastic <33279059+Sheepotastic@users.noreply.github.com>
2017-11-01 16:46:18 +0200

    Add files via upload

--- js/status.js
+++ js/status.js
@@ -1,12 +1,11 @@
 // For use with Node.js
 var ms = require('./minestat');
-ms.init('minecraft.dilley.me', 25565, function(result)
+ms.init('sheepotastic.shservers.net', 25568, function(result)
 {
   console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
   if(ms.online)
   {
     console.log("Server is online running version " + ms.version + " with " + ms.current_players + " out of " + ms.max_players + " players.");
-    console.log("Message of the day: " + ms.motd);
   }
   else
   {
